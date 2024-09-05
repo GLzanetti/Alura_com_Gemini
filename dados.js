83f3ebd3f929bb02b9e83e041ff4b62ac96bf2a2
Gabriel Zanetti <140424631+GLzanetti@users.noreply.github.com>
2024-09-05 18:14:14 -0300

    aula 3

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo : "Java Script",
+        descricao : "JavaScript é a linguagem que dá vida às páginas web, tornando-as interativas e dinâmicas.",
+        link : "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+    },
+    {
+        titulo : "Python",
+        descricao : "Python é uma linguagem de alto nível, popular e versátil, ideal para diversos tipos de projetos.",
+        link : "https://www.python.org/"
+    },
+    {
+        titulo : "Java",
+        descricao : "Java é uma linguagem orientada a objetos, robusta e portátil, utilizada em diversas áreas do desenvolvimento de software.",
+        link : "https://www.oracle.com/java/technologies/javase/"
+    },
+    {
+        titulo: "C#",
+        descricao: "Uma linguagem orientada a objetos desenvolvida pela Microsoft, amplamente utilizada para desenvolvimento de aplicativos .NET.",
+        link: "https://docs.microsoft.com/en-us/dotnet/csharp/"
+      },
+      {
+        titulo: "C++",
+        descricao: "Uma linguagem de programação de propósito geral, conhecida por sua eficiência e desempenho, utilizada em diversas áreas, como desenvolvimento de jogos e sistemas operacionais.",
+        link: "https://en.cppreference.com/"
+      },
+      {
+        titulo: "PHP",
+        descricao: "Uma linguagem de script principalmente utilizada para desenvolvimento web back-end, conhecida por sua facilidade de uso e integração com bancos de dados.",
+        link: "https://www.php.net/"
+      },
+]
